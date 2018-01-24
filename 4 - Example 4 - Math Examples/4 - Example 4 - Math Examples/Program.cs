767cc2f1cb6b8793a00d9cab2ca80ed850388669
Bruno Almeida <dbrunob@gmail.com>
2018-01-24 15:49:49 -0500

    Add files via upload

---
+++ 4 - Example 4 - Math Examples/4 - Example 4 - Math Examples/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_4
+{
+    class Program
+    {
+        //Method to check if the user is entering an integer number
+        public static string CheckString(string input)
+        {
+            bool result;
+            int number;
+            result = int.TryParse(input, out number);
+            while (!result)
+            {
+                Console.WriteLine("Please type an integer number");
+                input = Console.ReadLine();
+                result = int.TryParse(input, out number);
+            }
+            return input;
+        }
+
+        //Method for option 1 - even numbers
+        static void EvenNumber()
+        {
+            string stringNumbers;
+            int numbers, n, even;
+            even = 0;
+
+            Console.WriteLine("How many even numbers do you want?");
+            stringNumbers = Console.ReadLine();
+            stringNumbers = CheckString(stringNumbers);
+            numbers = int.Parse(stringNumbers);
+            //While to make sure the user will tipe a number greater than zero 
+            while (numbers < 1)
+            {
+                Console.WriteLine("Please, type a number greater than zero");
+                stringNumbers = Console.ReadLine();
+                stringNumbers = CheckString(stringNumbers);
+                numbers = int.Parse(stringNumbers);
+            }
+            Console.WriteLine(""); //This line is for esthetic purposes only 
+            //"For" loop to print the numbers 
+            for (n = 0; n < numbers; n++)
+            {
+                Console.WriteLine(even);
+                even = even + 2;
+            }
+
+        }
+
+        //Method for option 2 - Perfect square
+        static void PerfectSquare()
+        {
+            string stringOption;
+            int option, square;
+            square = 0;
+            option = 0;
+            while (option != 2)
+            {
+                square++;
+                Console.WriteLine("\n" + Math.Pow(square, 2));
+                Console.WriteLine("\nTo continue press 1, to get back to Main Menu press 2");
+                stringOption = Console.ReadLine();
+                stringOption = CheckString(stringOption);
+                option = int.Parse(stringOption);
+                while (option < 1 || option > 2)
+                {
+                    Console.WriteLine("Please enter 1 to continue or 2 to get back to Main menu");
+                    stringOption = Console.ReadLine();
+                    stringOption = CheckString(stringOption);
+                    option = int.Parse(stringOption);
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string stringMenu; //Variables declaration
+            int menu;
+            menu = 0;
+            do //Loop to the menu options
+            {
+                Console.WriteLine("\nPlease type the number of your choice:");
+                Console.WriteLine("\n1-Display even numbers \n2-Display perfect squares \n3-Exit the program\n");
+                stringMenu = Console.ReadLine();
+                //Call the method to check if the user is entering an integer number
+                stringMenu = CheckString(stringMenu);
+                menu = int.Parse(stringMenu);
+                //While to make sure the user do not enter a number out of the menu range
+                while (menu < 1 || menu > 3)
+                {
+                    Console.WriteLine("\nPlease type a number between 1 and 3");
+                    Console.WriteLine("\n1-Display even numbers \n2-Display perfect squares \n3-Exit the program\n");
+                    stringMenu = Console.ReadLine();
+                    stringMenu = CheckString(stringMenu);
+                    menu = int.Parse(stringMenu);
+                }
+                //Switch to call the proper method
+                switch (menu)
+                {
+                    case 1:
+                        EvenNumber();
+                        break;
+                    case 2:
+                        PerfectSquare();
+                        break;
+                }
+
+            } while (menu != 3);
+
+
+        }
+    }
+}
---
+++ 4 - Example 4 - Math Examples/README.txt
@@ -0,0 +1,16 @@
+Example 4 - Math Examples 
+
+You are creating a console based math program. This program will be shown as a menu with several options. The menu should be created with a do loop.
+The first option is to display a list of even numbers beginning at 0. Once this option is selected, the program asks the user for the number of even numbers to display. After the user provides this input, all the values should be displayed at once. This option should be created using a "for" loop.
+
+The second option is to display a sequence of perfect squares. The program should begin by printing the square of 1, and ask the user if they to continue or stop and return to the original menu. If the user does not want to quit, the next square is printed. This continues until the user wishes to end the sequence. This option should be created using a while loop.
+
+The third option is to exit the program. Any invalid option entered by the user should prompt the menu to reappear and ask the same three options.
+Any numeric entry by the user should not crash if the user enters a string.
+
+Your program must:
+a. Use the appropriate loop for each segment. 
+b. Encapsulate the logic of options 1 and 2 each within their own method. 
+c. The functionality of the menu is correct as described. 
+d. Proper error handling. 
+e. Adhere to coding standards as described by the course. 
